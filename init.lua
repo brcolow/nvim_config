bae46292e4bf6e04909edcfb1c869f8453b70fa3
unknown <mike.ennen@gmail.com>
2023-09-24 17:30:38 -0700

    Move JDTLS which key setup to ftplugin/java.lua.

--- init.lua
+++ init.lua
@@ -35,7 +35,6 @@ require("lazy").setup({
     }
   },
   {
-    -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
       'L3MON4D3/LuaSnip',
@@ -44,9 +43,7 @@ require("lazy").setup({
     },
   },
   {
-    -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
     opts = {
       options = {
         icons_enabled = false,
@@ -57,7 +54,6 @@ require("lazy").setup({
     },
   },
   {
-    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
@@ -139,49 +135,6 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
-local status_ok, which_key = pcall(require, "which-key")
-if not status_ok then return end
-
-local mappings = {
-  c = {
-    name = "Code",
-    o = { "<Cmd>lua require('jdtls').organize_imports()<CR>", "Organize Imports" },
-    v = { "<Cmd>lua require('jdtls').extract_variable()<CR>", "Extract Variable" },
-    c = { "<Cmd>lua require('jdtls').extract_constant()<CR>", "Extract Constant" },
-    t = { "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", "Test Method" },
-    T = { "<Cmd>lua require'jdtls'.test_class()<CR>", "Test Class" },
-    u = { "<Cmd>JdtUpdateConfig<CR>", "Update Config" },
-    a = { "<Cmd>lua AttachDebugLocalhost()<CR>", "Attach Debugger" },
-  },
-}
-
-local vmappings = {
-  c = {
-    name = "Code",
-    v = { "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", "Extract Variable" },
-    c = { "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", "Extract Constant" },
-    m = { "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", "Extract Method" },
-  },
-}
-
-which_key.register(mappings, {
-  mode = "n", -- NORMAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-})
-
-which_key.register(vmappings, {
-  mode = "v", -- VISUAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-})
 
 vim.o.hlsearch = false
 vim.wo.number = true
